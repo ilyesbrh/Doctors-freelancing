9070b8809ed9b3b4f36ac4dc1ea2826942f7fa5d
Hichem Ahmed <hichem2h@gmail.com>
2020-11-20 16:21:34 +0100

    Add alert to register

--- doccure-mobile/pwa/scripts/register.js
+++ doccure-mobile/pwa/scripts/register.js
@@ -18,15 +18,24 @@ $('#register-btn').on('click', async function () {
     }
 
     console.log(data);
+    try {
+        let response = await auth.register(data);
 
-    let response = await auth.register(data);
+        if (response) {
 
-    if (response) {
+            var params = new URLSearchParams();
+            params.append("phone", data.phone);
 
-        var params = new URLSearchParams();
-        params.append("phone", data.phone);
+            window.location.href = 'confirm-account.html?' + params.toString();
+        }
+        
+    } catch (error) {
 
-        window.location.href = 'confirm-account.html?' + params.toString();
+        swal({
+            title: "Can't Register!",
+            text: "Please verify your inputs!",
+            icon: "error",
+        });
     }
 
     $("#register-btn").text("Register");
